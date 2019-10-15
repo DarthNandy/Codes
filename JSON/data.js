f332d7c35b1633502e64e8672beb6d1694cc4d7c
Nandish <Ugly_Executioner@users.noreply.github.com>
2019-10-15 11:36:03 +0530

    Second Commit

---
+++ JSON/data.js
@@ -0,0 +1,145 @@
+var Recipe = [{
+    id: '101',
+    name: 'Biryani',
+    type: 'Indian',
+    image: '/Images/biryani.jpg',
+    info: 'Biryani, also known as biriyani, biriani, birani or briyani, is a mixed rice dish with its origins among the Silvassa people of the Indian subcontinent. It can be compared to mixing a curry, later combining it with semi-cooked rice separately.',
+    ing: 'Onion,tomato',
+    display: 'none'
+
+    
+    
+},
+{
+	id: '102',
+    name: 'Aloo paratha',
+    type: 'Indian',
+    image: '/Images/aloo.jpg',
+    info: 'Aloo Paratha is a bread dish originating from the Indian subcontinent;. The recipe is one of the most popular breakfast dishes throughout western, central and northern regions of India as well as in Pakistan.',
+    ing: 'Onion,tomato',
+    display: 'none'
+   
+},
+{
+	id: '103',
+    name: 'Samosa',
+    type: 'Indian',
+    image: '/Images/samosa.jpg',
+    info: 'A samosa is a fried or baked pastry with a savoury filling, such as spiced potatoes, onions, peas, meat, or lentils. It may take different forms, including triangular, cone, or half-moon shapes, depending on the region.',
+    ing: 'Onion,tomato',
+    display: 'none'
+    
+},
+{
+    id: '201',
+    name: 'Pizza',
+    type: 'Italian',
+    image: '/Images/pizza.jpg',
+    info: 'Pizza is a savory dish of Italian origin, consisting of a usually round, flattened base of leavened wheat-based dough topped with tomatoes, cheese, and various other ingredients baked at a high temperature, traditionally in a wood-fired oven. A small pizza is sometimes called a pizzetta. ',
+    ing: 'Onion,tomato',
+    display: 'none'
+
+
+},
+{
+    id: '202',
+    name: 'Garlic bread',
+    type: 'Italian',
+    image: '/Images/garlic.jpg',
+    info: 'Garlic bread consists of bread, topped with garlic and olive oil or butter and may include additional herbs, such as oregano or chives. It is then either grilled or broiled until toasted or baked in a conventional or bread oven.',
+    ing: 'Onion,tomato',
+    display: 'none'
+},
+{
+    id: '203',
+    name: 'Pasta',
+    type: 'Italian',
+    image: '/Images/pasta.jpg',
+    info: 'Pasta is a type of food typically made from an unleavened dough of durum wheat flour mixed with water or eggs, and formed into sheets or various shapes, then cooked by boiling or baking.',
+    ing: 'Onion,tomato',
+    display: 'none'
+},
+{
+    id: '301',
+    name: 'Noodles',
+    type: 'Chinese',
+    image: '/Images/noodles.png',
+    info: 'Noodles are a type of food made from unleavened dough which is rolled flat and cut, or extruded, into long strips or strings. Noodles can be refrigerated for short-term storage or dried and stored for future use. Noodles are usually cooked in boiling water, sometimes with cooking oil or salt added.',
+    ing: 'Onion,tomato',
+    display: 'none'
+
+},
+{
+    id: '302',
+    name: 'Momos',
+    type: 'Chinese',
+    image: '/Images/momos.jpg',
+    info: 'Momo is a type of South Asian dumpling, popular across the Indian subcontinent and the Himalayan regions of broader South Asia. Momos are native to Tibet, Bhutan, Nepal, North Indian region of Ladakh, Northeast Indian regions of Sikkim, Assam, and Arunachal Pradesh, and Darjeeling.',
+    ing: 'Onion,tomato',
+    display: 'none'
+},
+{
+    id: '303',
+    name: 'Fried Rice',
+    type: 'Chinese',
+    image: '/Images/fried.jpg',
+    info: 'Fried rice is a dish of cooked rice that has been stir-fried in a wok or a frying pan and is usually mixed with other ingredients such as eggs, vegetables, seafood, or meat. It is often eaten by itself or as an accompaniment to another dish.',
+    ing: 'Onion,tomato',
+    display: 'none'
+},
+{
+    id: '401',
+    name: 'French fries',
+    type: 'French',
+    image: '/Images/fries.jpg',
+    info: 'French fries, or simply fries, chips, finger chips, or french-fried potatoes, are batonnet or allumette-cut deep-fried potatoes.',
+    ing: 'Onion,tomato',
+    display: 'none'
+
+},
+{
+    id: '402',
+    name: 'Ratatouille',
+    type: 'French',
+    image: '/Images/rat.jpg',
+    info: 'Ratatouille is a French Provençal stewed vegetable dish, originating in Nice, and sometimes referred to as ratatouille niçoise.',
+    ing: 'Onion,tomato',
+    display: 'none'
+},
+{
+    id: '403',
+    name: 'French toast',
+    type: 'French',
+    image: '/Images/toast.jpg',
+    info: 'French toast is a dish made of sliced bread soaked in eggs and milk, then fried. Alternative names and variants include eggy bread, Bombay toast, German toast, gypsy toast, poor knights, and torrija.',
+    ing: 'Onion,tomato',
+    display: 'none'
+},
+{
+    id: '501',
+    name: 'Kimchi fried rice',
+    type: 'Korean',
+    image: '/Images/kimichi.jpg',
+    info: 'Kimchi fried rice or kimchi-bokkeum-bap is a variety of bokkeum-bap, a popular dish in Korea. Kimchi fried rice is made primarily with kimchi and rice, along with other available ingredients, such as diced vegetables or meats like spam.',
+    ing: 'Onion,tomato',
+    display: 'none'
+},
+{
+    id: '502',
+    name: 'Korean tofu',
+    type: 'Korean',
+    image: '/Images/tofu.jpg',
+    info: 'Tofu, also known as bean curd, is a food prepared by coagulating soy milk and then pressing the resulting curds into solid white blocks of varying softness; it can be silken, soft, firm, or extra firm. Beyond these broad categories, there are many varieties of tofu.',
+    ing: 'Onion,tomato',
+    display: 'none'
+},
+{
+    id: '503',
+    name: 'Korean Bibimbap',
+    type: 'Korean',
+    image: '/Images/bibi.jpg',
+    info: 'Bibimbap, sometimes romanized as bi bim bap or bi bim bop, is a Korean rice dish. The term “bibim” means mixing various ingredients, while the “bap” noun refers to rice. Bibimbap is served as a bowl of warm white rice topped with namul or kimchi and gochujang, soy sauce, or doenjang.',
+    ing: 'Onion,tomato',
+    display: 'none'
+}
+];
